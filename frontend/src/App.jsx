5610fb3ed5f47e33ca6ce4caa8f4f0a8b255ae75
Sadaf-A <sadaf.ahmed.in@hotmail.com>
2025-03-24 17:10:44 +0530

    fix: correct import path for HomePage component and update route path in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import HomePage from "./pages/HomePage.jsx";
+import HomePage from "./pages/Homepage.jsx";
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import WasteSortPage from './pages/WasteSortPage';
@@ -20,7 +20,7 @@ function App() {
         <div className="layout-container flex h-full grow flex-col">
           <Header />
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/homepage" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
              <Route path="/wastesort" element={<WasteSortPage />} />
